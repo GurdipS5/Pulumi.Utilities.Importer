b9abb47fa49ad75917787c46f4fbdf394d46a66b
GurdipS5 <gurdip.sira@gmail.com>
2024-04-23 03:53:27 +0100

    docs: :memo: s
    s

--- Pulumi.Utilities.Importer/Program.cs
+++ Pulumi.Utilities.Importer/Program.cs
@@ -4,13 +4,13 @@ using CliWrap;
 
 try
 {
+    //
     string workingDir = "";
     string filePath = Path.Combine(workingDir, "import.json");
     File.WriteAllText(filePath, args[0]);
 
     var result = await Cli.Wrap("pulumi")
         .WithArguments(["import", $"-f {filePath}"])
-
         .WithWorkingDirectory("work/dir/path")
         .ExecuteAsync();
 
