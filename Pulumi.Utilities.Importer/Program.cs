4e15e393d8aefca4f08181898ef78f38ceb4b8af
GurdipS5 <gurdip.sira@gmail.com>
2024-04-23 02:30:17 +0100

    feat: :sparkles: s
    s
    
    BREAKING CHANGE: s

---
+++ Pulumi.Utilities.Importer/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CliWrap;
+
+try
+{
+    string workingDir = "";
+    string filePath = Path.Combine(workingDir, "import.json");
+    File.WriteAllText(filePath, args[0]);
+
+    var result = await Cli.Wrap("pulumi")
+        .WithArguments(["import", $"-f {filePath}"])
+
+        .WithWorkingDirectory("work/dir/path")
+        .ExecuteAsync();
+
+}
+
+catch
+{
+
+}
